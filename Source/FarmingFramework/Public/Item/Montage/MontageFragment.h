b69575cb766104f081f425b74026c5aa162a8d9b
Junghyeon0710 <sie08357@naver.com>
2025-07-07 16:12:59 +0900

    feat: MontageFragment 클래스 추가

---
+++ Source/FarmingFramework/Public/Item/Montage/MontageFragment.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item/FarmItemFragment.h"
+#include "MontageFragment.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FARMINGFRAMEWORK_API UMontageFragment : public UFarmItemFragment
+{
+    GENERATED_BODY()
+};
