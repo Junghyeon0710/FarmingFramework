26df96d2ec1fb7da371cecb09b78d525c524ff5e
Junghyeon0710 <sie08357@naver.com>
2025-07-25 21:59:11 +0900

    feat: 몽타주 재생중에 인터랙트 안되게 설정

--- Source/FarmingFramework/Private/Item/Components/Farm_ItemComponent.cpp
+++ Source/FarmingFramework/Private/Item/Components/Farm_ItemComponent.cpp
@@ -44,7 +44,10 @@ void UFarm_ItemComponent::TryUseItem()
 {
 	for (UFarmItemFragment* Fragment : Fragments)
 	{
-		Fragment->OnInteract();
+	    if (Fragment->CanInteract())
+	    {
+		    Fragment->OnInteract();
+	    }
 	}
 }
 
