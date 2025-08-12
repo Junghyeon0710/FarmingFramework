e38fee60f6e5a7391a1880bc7d5c90b5c3d89b44
Junghyeon0710 <sie08357@naver.com>
2025-08-12 18:15:27 +0900

    feat: 내비게이션 사이즈 구하는 함수 추가 - 내비게이션 사이즈 구하는 함수 추가 - 내비게이션 사이즈 기준으로 배치 가능 비율 구하기 함수 추가

--- Source/FarmingFramework/Private/Actors/Spawner/SpawnManager.cpp
+++ Source/FarmingFramework/Private/Actors/Spawner/SpawnManager.cpp
@@ -1,16 +1,41 @@
 ﻿
 #include "Actors/Spawner/SpawnManager.h"
 
+#include "NavigationData.h"
+#include "NavigationSystem.h"
+#include "NavMesh/NavMeshBoundsVolume.h"
+#include "NavMesh/RecastNavMesh.h"
+
 
 ASpawnManager::ASpawnManager()
 {
     PrimaryActorTick.bCanEverTick = false;
+
 }
 
 void ASpawnManager::BeginPlay()
 {
     Super::BeginPlay();
 
+}
+
+FVector ASpawnManager::GetNavVolumeSize() const
+{
+    checkf(NavMeshBoundsVolume,TEXT("NavMeshBoundsVolume is NULL"));
+
+    FVector Origin = FVector(0, 0, 0);
+    FVector Extent = FVector(0, 0, 0);
+
+    NavMeshBoundsVolume->GetActorBounds(false, Origin, Extent);
+
+    return Extent * 2;
+}
+
+int32 ASpawnManager::CalculateSpawnCountByFarmSizePercentage(float SpawnRatePercent) const
+{
+    FVector TileSize = GetNavVolumeSize() / 100;
+
+    return TileSize.X * TileSize.Y * SpawnRatePercent;
 }
 
 
--- Source/FarmingFramework/Public/Actors/Spawner/SpawnManager.h
+++ Source/FarmingFramework/Public/Actors/Spawner/SpawnManager.h
@@ -6,6 +6,20 @@
 #include "GameFramework/Actor.h"
 #include "SpawnManager.generated.h"
 
+class ANavMeshBoundsVolume;
+
+USTRUCT(BlueprintType)
+struct FSpawnData
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    TSoftClassPtr<AActor> ClassRef;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float SpawnRatePerFarmSize = 0.1f; // 농작지 크기 비례 비율
+};
+
 class ANavigationData;
 
 UCLASS()
@@ -24,5 +38,14 @@ private:
     UPROPERTY(EditAnywhere, Category = "Navigation")
     TObjectPtr<ANavigationData> NavigationData;
 
+    UPROPERTY(EditAnywhere, Category = "Navigation")
+    TObjectPtr<ANavMeshBoundsVolume> NavMeshBoundsVolume;
 
+    FVector GetNavVolumeSize() const;
+
+private:
+    UPROPERTY(EditAnywhere, Category = "Spawn")
+    TArray<FSpawnData> SpawnData;
+
+    int32 CalculateSpawnCountByFarmSizePercentage(float SpawnRatePercent) const;
 };
