fab9e327cab91411fe18b74d9f2be00348749af7
Junghyeon0710 <sie08357@naver.com>
2025-08-17 17:44:49 +0900

    feat: 데이테에셋 정의 추가

--- Source/FarmingFramework/Public/Actors/Spawner/Data/FarmSpawnDataAsset.h
+++ Source/FarmingFramework/Public/Actors/Spawner/Data/FarmSpawnDataAsset.h
@@ -6,11 +6,37 @@
 #include "Engine/DataAsset.h"
 #include "FarmSpawnDataAsset.generated.h"
 
-/**
- * 
- */
+
+USTRUCT(BlueprintType)
+struct FSpawnClassData
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    TArray<TSoftClassPtr<AActor>> ClassRef;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float Probability = -1.0f;
+};
+
+USTRUCT(BlueprintType)
+struct FSpawnData
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    FSpawnClassData SpawnClassData;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float SpawnRatePerFarmSize = 0.1f;
+};
+
 UCLASS()
 class FARMINGFRAMEWORK_API UFarmSpawnDataAsset : public UDataAsset
 {
     GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TArray<FSpawnData> SpawnData;
 };
