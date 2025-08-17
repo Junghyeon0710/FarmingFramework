2b83946c0b32389a123ce4de228199e15ea85fb3
Junghyeon0710 <sie08357@naver.com>
2025-08-17 17:38:23 +0900

    feat: 데이터에셋 클래스 추가

---
+++ Source/FarmingFramework/Public/Actors/Spawner/Data/FarmSpawnDataAsset.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "FarmSpawnDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FARMINGFRAMEWORK_API UFarmSpawnDataAsset : public UDataAsset
+{
+    GENERATED_BODY()
+};
