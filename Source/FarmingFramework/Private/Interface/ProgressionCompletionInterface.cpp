f3d0979113428ea295ab9686453b88206e0bcb6d
Junghyeon0710 <sie08357@naver.com>
2025-09-19 16:22:54 +0900

    feat: IProgressionCompletionInterface인터페이스 추가

---
+++ Source/FarmingFramework/Private/Interface/ProgressionCompletionInterface.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interface/ProgressionCompletionInterface.h"
+
+
+// Add default functionality here for any IProgressionCompletionInterface functions that are not pure virtual.
