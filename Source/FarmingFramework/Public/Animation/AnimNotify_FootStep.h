5d7188181b6f4fce398ab0e4fdcc8a0edc7a9102
Junghyeon0710 <sie08357@naver.com>
2026-01-04 22:57:07 +0900

    feat: 풋스템 노티파이 추가

---
+++ Source/FarmingFramework/Public/Animation/AnimNotify_FootStep.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_FootStep.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FARMINGFRAMEWORK_API UAnimNotify_FootStep : public UAnimNotify
+{
+    GENERATED_BODY()
+};
