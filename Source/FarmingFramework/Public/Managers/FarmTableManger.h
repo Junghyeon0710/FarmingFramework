b310950eee8e4b50b702d38f1885bc376466f782
Junghyeon0710 <sie08357@naver.com>
2026-01-25 16:22:59 +0900

    Feat: 테이블 매니저 클래스 추가

---
+++ Source/FarmingFramework/Public/Managers/FarmTableManger.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "FarmTableManger.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class FARMINGFRAMEWORK_API UFarmTableManger : public UGameInstanceSubsystem
+{
+    GENERATED_BODY()
+};
