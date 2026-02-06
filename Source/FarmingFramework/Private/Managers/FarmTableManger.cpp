901b5024364014ebf1a58d8aa4f225aadccc4fcf
Junghyeon0710 <sie08357@naver.com>
2026-02-07 01:22:57 +0900

    feat: 테이블 추가

--- Source/FarmingFramework/Private/Managers/FarmTableManger.cpp
+++ Source/FarmingFramework/Private/Managers/FarmTableManger.cpp
@@ -2,3 +2,13 @@
 
 
 #include "Managers/FarmTableManger.h"
+
+#include "Data/CropData.h"
+
+void UFarmTableManger::Initialize(FSubsystemCollectionBase& Collection)
+{
+    Super::Initialize(Collection);
+
+    LoadDataTableToMap<FCropData>(TEXT("/Script/Engine.DataTable'/Game/Project/Farm/Data/DT_CropData.DT_CropData'"), CropDataMap);
+    UE_LOG(LogTemp, Warning, TEXT("FarmTableManger Initialized"));
+}
--- Source/FarmingFramework/Public/Managers/FarmTableManger.h
+++ Source/FarmingFramework/Public/Managers/FarmTableManger.h
@@ -6,6 +6,7 @@
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "FarmTableManger.generated.h"
 
+struct FCropData;
 /**
  *
  */
@@ -13,4 +14,54 @@ UCLASS()
 class FARMINGFRAMEWORK_API UFarmTableManger : public UGameInstanceSubsystem
 {
     GENERATED_BODY()
+
+public:
+
+    /** Begin USubSystem */
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+    /** ~End USubsystem */
+
+public:
+
+    TMap<int32, TSharedPtr<FCropData>> CropDataMap;
+
+
+
+
+public:
+
+template<typename RowStruct>
+bool LoadDataTableToMap(const FString& DataTablePath, TMap<int32, TSharedPtr<RowStruct>>& OutDataMap)
+    {
+        static_assert(TIsDerivedFrom<RowStruct, FTableRowBase>::IsDerived,"RowStruct must derive from FTableRowBase");
+
+        const UDataTable* DataTable = LoadObject<UDataTable>(nullptr, *DataTablePath);
+        if (!DataTable)
+        {
+            UE_LOG(LogTemp, Warning, TEXT("Failed to load DataTable : %s"), *DataTablePath);
+            return false;
+        }
+
+        TArray<RowStruct*> Rows;
+        DataTable->GetAllRows(TEXT("LoadDataTableToMap"), Rows);
+
+        const TArray<FName> RowNames = DataTable->GetRowNames();
+
+        if (Rows.Num() != RowNames.Num())
+        {
+            UE_LOG(LogTemp, Warning, TEXT("Row count mismatch : %s"), *DataTablePath);
+            return false;
+        }
+
+        OutDataMap.Empty(Rows.Num());
+
+        for (int32 Index = 0; Index < Rows.Num(); ++Index)
+        {
+            const int32 ID = FCString::Atoi(*RowNames[Index].ToString());
+            OutDataMap.Emplace(ID, MakeShared<RowStruct>(*Rows[Index]));
+        }
+
+        return true;
+    }
+
 };
