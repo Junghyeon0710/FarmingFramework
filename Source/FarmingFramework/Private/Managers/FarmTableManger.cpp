dec886d49a68efe9b418d36f4a68d59b6a8d2369
안중현 <133496610+Junghyeon0710@users.noreply.github.com>
2026-02-07 13:57:54 +0900

    Feat: 테이블 기능 추가 (#24)
    * Feat: Crop데이터 추가, Crop리소스 추가
    
    * Feat: 테이블 매니저 클래스 추가
    
    * feat: 테이블 추가

---
+++ Source/FarmingFramework/Private/Managers/FarmTableManger.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Managers/FarmTableManger.h"
+
+#include "Data/CropData.h"
+
+void UFarmTableManger::Initialize(FSubsystemCollectionBase& Collection)
+{
+    Super::Initialize(Collection);
+
+    LoadDataTableToMap<FCropData>(TEXT("/Script/Engine.DataTable'/Game/Project/Farm/Data/DT_CropData.DT_CropData'"), CropDataMap);
+    UE_LOG(LogTemp, Warning, TEXT("FarmTableManger Initialized"));
+}
---
+++ Source/FarmingFramework/Public/Data/CropData.h
@@ -0,0 +1,71 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CropData.generated.h"
+
+/**
+ *
+ */
+USTRUCT()
+struct FCropData : public FTableRowBase
+{
+    GENERATED_BODY()
+
+
+    UPROPERTY(EditAnywhere, Category = "Crop Data")
+    FString Name;
+
+    /** 작물 성장 그룹 ID */
+    UPROPERTY(EditAnywhere, Category = "Crop Data")
+    int32 CropGrowthID;
+
+    /** 수확에 필요한 재배 일수 */
+    UPROPERTY(EditAnywhere, Category = "Crop Data")
+    int32 GrowingDays;
+
+    /** 작물 재배 시 일반 획득 보상 */
+    UPROPERTY(EditAnywhere, Category = "Crop Data")
+    int32 CropRewardIDNormal;
+
+    /** 작물 재배 시 대성공 획득 보상 */
+    UPROPERTY(EditAnywhere, Category = "Crop Data")
+    int32 CropRewardIDLucky;
+
+    /** 작물 건강에 피해주지 않는 온도 최소값 */
+    UPROPERTY(EditAnywhere, Category = "Crop Data")
+    int32 TemperatureMin;
+
+    /** 작물 건강에 피해주지 않는 온도 최대값 */
+    UPROPERTY(EditAnywhere, Category = "Crop Data")
+    int32 TemperatureMax;
+
+    /** 작물 건강 시작 최소값 */
+    UPROPERTY(EditAnywhere, Category = "Crop Data")
+    int32 InitCropHealthMin;
+
+    /** 작물 건강 시작 최대값 */
+    UPROPERTY(EditAnywhere, Category = "Crop Data")
+    int32 InitCropHealthMax;
+
+    /** 대성공을 위해 필요한 작물 건강 값 */
+    UPROPERTY(EditAnywhere, Category = "Crop Data")
+    int32 NeedCropHealthforLucky;
+
+    /** 대성공 확률 */
+    UPROPERTY(EditAnywhere, Category = "Crop Data")
+    int32 LuckyRewardRate;
+
+    /** 병충해 악영향 유무 */
+    UPROPERTY(EditAnywhere, Category = "Crop Data")
+    bool PestsRisk;
+
+    /** 야생동물 악영향 유무 */
+    UPROPERTY(EditAnywhere, Category = "Crop Data")
+    bool AnimalRisk;
+
+    /** 야간 농사 미진행 시 악영향 유무 */
+    UPROPERTY(EditAnywhere, Category = "Crop Data")
+    bool NightRisk;
+};
